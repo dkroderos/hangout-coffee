28e493889ec0a401301db7dd7d4de89948f80855
David King Roderos <dk.roderos@proton.me>
2024-10-10 15:21:06 +0800

    Add home cart

---
+++ src/home/home.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const cartCountElement = document.getElementById("cart-count");
+  cartCountElement.innerText = getCartCount();
+});
+
+function getCartCount() {
+  let cartCoffee = JSON.parse(localStorage.getItem("cartCoffee") || "[]");
+  let cartFood = JSON.parse(localStorage.getItem("cartFood") || "[]");
+  let cartRegular = JSON.parse(localStorage.getItem("cartRegular") || "[]");
+  let cartSpecial = JSON.parse(localStorage.getItem("cartSpecial") || "[]");
+
+  let totalQuantity = 0;
+
+  const calculateQuantity = (cart) => {
+    return cart.reduce((sum, item) => sum + item.quantity, 0);
+  };
+
+  totalQuantity += calculateQuantity(cartCoffee);
+  totalQuantity += calculateQuantity(cartFood);
+  totalQuantity += calculateQuantity(cartRegular);
+  totalQuantity += calculateQuantity(cartSpecial);
+
+  return totalQuantity;
+}
